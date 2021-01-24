fa37217eaaa5119ec8e89a63c576a6a7f0f10951
Hao Jiang <joshtrick@outlook.com>
2021-01-24 15:57:30 +0800

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
 let myImage = document.querySelector('img');
 
-myImage.onclick = funciton() {
-  let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/logo-size.jpg')
-  {
-    myImage.setAttribut('src', 'images/logo-size-invert.jpg');
-  } else {
-    myImage.setAttribut('src', 'images/logo-size.jpg');
-  }
-}
+//myImage.onclick = funciton()
+//{
+//  let mySrc = myImage.getAttribute('src');
+//  if(mySrc === 'images/logo-size.jpg')
+//  {
+//    myImage.setAttribut('src', 'images/logo-size-invert.jpg');
+//  }
+//  else
+//  {
+//    myImage.setAttribut('src', 'images/logo-size.jpg');
+//  }
+//}
+
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
